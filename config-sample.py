891c03cbbaa2db69cfb4fb7966aa8a9f61b5145e
Francis Tseng <f+accounts@frnsys.com>
2014-11-16 14:30:08 -0500

    added collect tweet script

--- config-sample.py
+++ config-sample.py
@@ -3,3 +3,8 @@ SECRET_KEY = 'some-passphrase'
 
 AUTH_USER = 'admin'
 AUTH_PASS = 'password'
+
+# You can get these by creating a new app at
+# https://apps.twitter.com/
+TWITTER_CONSUMER_KEY = 'fill_me_in'
+TWITTER_CONSUMER_SECRET = 'fill_me_in'
